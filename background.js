844d29463412c98e012a641637b11f2c248b13e2
sushiSushanshi <sushisushanshi13@gmail.com>
2021-06-08 23:29:15 +0530

    initial commit

---
+++ background.js
@@ -0,0 +1,15 @@
+var contextMenuItem ={
+    "id":"compress",
+    "title":"compress",
+    "contexts": ["all"]
+};
+
+//for creating the context in the list
+chrome.contextMenus.create(contextMenuItem);
+
+//action to be performed after clicking the added context
+chrome.contextMenus.onClicked.addListener(function(clickData){
+    if(clickData.menuItemId){
+           alert("work done");
+    }
+})
