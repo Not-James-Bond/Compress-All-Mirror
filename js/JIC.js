329bfe2bf8c2eccc9a09779ef5620f0d70a44d2a
Not-James-Bond <chaitanyasingh92@yahoo.com>
2021-06-12 10:43:44 +0530

    fixing bugs and adding few libraries

--- js/JIC.js
+++ js/JIC.js
@@ -1,29 +1,6 @@
-/*!
- * JIC JavaScript Library v2.0.2
- * https://github.com/brunobar79/J-I-C/
- *
- * Copyright 2016, Bruno Barbieri
- * Dual licensed under the MIT or GPL Version 2 licenses.
- *
- * Date: Tue Jul 11 13:13:03 2016 -0400
- */
 
-
-
-/**
- * Create the jic object.
- * @constructor
- */
 
 var jic = {
-        /**
-         * Receives an Image Object (can be JPG, PNG, or WEBP) and returns a new Image Object compressed
-         * @param {Image} source_img_obj The source Image Object
-         * @param {Integer} quality The output quality of Image Object
-         * @param {String} output format. Possible values are jpg, png, and webp
-         * @return {Image} result_image_obj The compressed Image Object
-         */
-
         compress: function(source_img_obj, quality, output_format){
              
              var mime_type;
@@ -45,21 +22,8 @@ var jic = {
              return result_image_obj;
         },
 
-        /**
-         * Receives an Image Object and upload it to the server via ajax
-         * @param {Image} compressed_img_obj The Compressed Image Object
-         * @param {String} The server side url to send the POST request
-         * @param {String} file_input_name The name of the input that the server will receive with the file
-         * @param {String} filename The name of the file that will be sent to the server
-         * @param {function} successCallback The callback to trigger when the upload is succesful.
-         * @param {function} (OPTIONAL) errorCallback The callback to trigger when the upload failed.
-	     * @param {function} (OPTIONAL) duringCallback The callback called to be notified about the image's upload progress.
-	     * @param {Object} (OPTIONAL) customHeaders An object representing key-value  properties to inject to the request header.
-         */
-
         upload: function(compressed_img_obj, upload_url, file_input_name, filename, successCallback, errorCallback, duringCallback, customHeaders){
 
-            //ADD sendAsBinary compatibilty to older browsers
             if (XMLHttpRequest.prototype.sendAsBinary === undefined) {
                 XMLHttpRequest.prototype.sendAsBinary = function(string) {
                     var bytes = Array.prototype.map.call(string, function(c) {
@@ -86,15 +50,13 @@ var jic = {
             var boundary = 'someboundary';
 
             xhr.setRequestHeader('Content-Type', 'multipart/form-data; boundary=' + boundary);
-		
-		// Set custom request headers if customHeaders parameter is provided
+
 		if (customHeaders && typeof customHeaders === "object") {
 			for (var headerKey in customHeaders){
 				xhr.setRequestHeader(headerKey, customHeaders[headerKey]);
 			}
 		}
-		
-		// If a duringCallback function is set as a parameter, call that to notify about the upload progress
+
 		if (duringCallback && duringCallback instanceof Function) {
 			xhr.upload.onprogress = function (evt) {
 				if (evt.lengthComputable) {  
--- js/launch.js
+++
@@ -1,7 +0,0 @@
-const url = "ui/comp.html";
-
-
-chrome.browserAction.onClicked.addListener(function(activeTab)
-{
-    chrome.tabs.create({ url: url });
-});
